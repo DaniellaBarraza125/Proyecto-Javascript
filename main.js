d8f221c8d35f3111319398cb04543e21e0fc2076
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-04-13 10:32:26 +0200

    showViews check

--- main.js
+++ main.js
@@ -15,27 +15,27 @@ const resultsDiv = document.getElementById ("resultsDiv")
 
 console.log(resultsNav, homeNav, quizNav);
 
-// const hideViews = (()=>{
-//     console.log("working booetch");
-//     homeDiv.classList.add("hide")
-//     quizDiv.classList.add("hide")
-//     resultsDiv.classList.add("hide")
-// })
+const hideViews = (()=>{
+    console.log("working booetch");
+    homeDiv.classList.add("hide")
+    quizDiv.classList.add("hide")
+    resultsDiv.classList.add("hide")
+})
 const showHome =()=>{
-    // hideViews()
+    hideViews()
     console.log("working");
     homeDiv.classList.remove("hide")
 }
 
 const showQuiz =()=>{
-    // hideViews()
+    hideViews()
     console.log("working");
 
     quizDiv.classList.remove("hide")
 }
 
 const showResults =()=>{
-    // hideViews()
+    hideViews()
     console.log("working");
 
     resultsDiv.classList.remove("hide")
@@ -46,79 +46,79 @@ quizNav.addEventListener("click",showQuiz)
 resultsNav.addEventListener("click",showResults)
 
 
-// const API = "https://mocki.io/v1/e97adce9-e6cb-4ad9-8f31-0e3528b59e43";
+const API = "https://mocki.io/v1/e97adce9-e6cb-4ad9-8f31-0e3528b59e43";
 
-// let quiz = [];
-// let answer = [];
+let quiz = [];
+let answer = [];
 
-// axios.get(API)
-// .then ((res)=> {quiz = res.data
-// //si llega a dejar de funcionar esta API y debo cambiar a la anterior recuerda que seria res.data.response  
-//     // showQuestion(quiz)
-//     quiz.map(quiz => {
-// //*crear vaiables para acceder mas facilmente al contenido. 
-//         correctAnswer = quiz.correct_answer
-//         incorrectAnswers = quiz.incorrect_answers
-//     console.log(correctAnswer); 
+axios.get(API)
+.then ((res)=> {quiz = res.data
+//si llega a dejar de funcionar esta API y debo cambiar a la anterior recuerda que seria res.data.response  
+    // showQuestion(quiz)
+    quiz.map(quiz => {
+//*crear vaiables para acceder mas facilmente al contenido. 
+        correctAnswer = quiz.correct_answer
+        incorrectAnswers = quiz.incorrect_answers
+    console.log(correctAnswer); 
         
-//     });
-//     console.log("log en axios=", quiz)
-//     console.log("trayend respuestas=", incorrectAnswers,correctAnswer);;
-// })
-// .catch((err)=>console.error(err))
+    });
+    console.log("log en axios=", quiz)
+    console.log("trayend respuestas=", incorrectAnswers,correctAnswer);;
+})
+.catch((err)=>console.error(err))
 
 
-// let currentQuestionIndex;
-// const startGame = (()=>{
-//     // console.log("funciono!");
-//     // console.log("respuesta incorrecta", incorrectAnswers)
-//     // console.log("correcta", correctAnswer); 
+let currentQuestionIndex;
+const startGame = (()=>{
+    // console.log("funciono!");
+    // console.log("respuesta incorrecta", incorrectAnswers)
+    // console.log("correcta", correctAnswer); 
 
 
-//     startButton.classList.add("hide");
-//     currentQuestionIndex = 0;
-//     questionContainerElement.classList.remove("hide");
-//     setNextQuestion();
-// })
+    startButton.classList.add("hide");
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove("hide");
+    setNextQuestion();
+})
 
-// // startGame();
-// startButton.addEventListener("click", startGame);
+// startGame();
+startButton.addEventListener("click", startGame);
 
-// const showQuestion = ((question)=>{
-// console.log("acceder a una pregunta=", quiz[1]);  
-// questionElement.innerText = `holi ${question.question}`;
-// //* problema, la respuesta correcta siempre esta en el mismo lugar, he reado un array para unirlas, el problema sera asignar el valor de correcta. concatener las variables, unsar la funcion math.random para darle un orden al azar
-// const correctAnswer = question.correct_answer;
-// const incorrectAnswers = question.incorrect_answers;
+const showQuestion = ((question)=>{
+console.log("acceder a una pregunta=", quiz[1]);  
+questionElement.innerText = `holi ${question.question}`;
+//* problema, la respuesta correcta siempre esta en el mismo lugar, he reado un array para unirlas, el problema sera asignar el valor de correcta. concatener las variables, unsar la funcion math.random para darle un orden al azar
+const correctAnswer = question.correct_answer;
+const incorrectAnswers = question.incorrect_answers;
 
-//     let allAnswers = [correctAnswer, ...incorrectAnswers];
-//     allAnswers.sort(() => Math.random() - 0.5);
-//     console.log("allanswer", allAnswers);
-
-
-//     allAnswers.forEach(answer => {
-//     const button = document.createElement("button");
-//     button.innerText = answer;
-//     button.innerText = answer;button.classList.add("answerButton")
-//     button.dataset.correct = false;    
-//     answerButtonsElement.appendChild(button);
-// });
-// // no entiendia porque no funcionaba check buttons y chatGPT me dio la opcion de poner answerButtonsElement.querySelectorAll("button") como argumento de la funcion. asi se inicia correctamnte
+    let allAnswers = [correctAnswer, ...incorrectAnswers];
+    allAnswers.sort(() => Math.random() - 0.5);
+    console.log("allanswer", allAnswers);
 
 
-//     checkButtons(answerButtonsElement.querySelectorAll("button"),correctAnswer);
+    allAnswers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerText = answer;
+    button.innerText = answer;button.classList.add("answerButton")
+    button.dataset.correct = false;    
+    answerButtonsElement.appendChild(button);
+});
+// no entiendia porque no funcionaba check buttons y chatGPT me dio la opcion de poner answerButtonsElement.querySelectorAll("button") como argumento de la funcion. asi se inicia correctamnte
 
-// })
-// const checkButtons =(answerButtons, correctAnswer)=>{
-//     answerButtons.forEach(button => {
-//         if (button.innerText === correctAnswer) {
-//             console.log("Botón correcto: ", button.innerText);
-//             button.dataset.correct = true;
-//         }
-//     });
-// }
 
-// function setNextQuestion() {
-//     // resetState();
-//     showQuestion(quiz[currentQuestionIndex]);
-// }
+    checkButtons(answerButtonsElement.querySelectorAll("button"),correctAnswer);
+
+})
+const checkButtons =(answerButtons, correctAnswer)=>{
+    answerButtons.forEach(button => {
+        if (button.innerText === correctAnswer) {
+            console.log("Botón correcto: ", button.innerText);
+            button.dataset.correct = true;
+        }
+    });
+}
+
+function setNextQuestion() {
+    // resetState();
+    showQuestion(quiz[currentQuestionIndex]);
+}
