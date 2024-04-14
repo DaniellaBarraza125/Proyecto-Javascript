d7e71d057874db57199e2f485cc464b78b88465a
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-04-14 11:23:41 +0200

    showResults check

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const homeDiv = document.getElementById("homeDiv")
 const quizDiv = document.getElementById("quizDiv")
 const resultsDiv = document.getElementById ("resultsDiv")
 const resultText = document.getElementById("resultText")
+const resultsTitle = document.getElementById("resultsTitle")
 const API = "https://mocki.io/v1/e97adce9-e6cb-4ad9-8f31-0e3528b59e43";
 
 let quiz = [];
@@ -149,15 +150,24 @@ const resetState =(()=>{
     answerButtonsElement.innerHTML = "";
 })
 const showResults =(()=>{
-   showResultsDiv()
+    showResultsDiv()
     finalScore = score; // Asigna el valor de score a finalScore
-    resultText.innerText = `Tu puntaje final es ${finalScore} . `; 
-    //  if (score == 9) {console.log("yeeeeii");
-    // }
-    // else {
-    //     console.log("owww");
-    // }
-})
+    if (finalScore >= 9 && finalScore <= 10) {
+        resultsTitle.innerText= `Felicidades, Parece que Te Gusta Tanto Friends como a Monica Limpiar
+        `
+        resultText.innerText = "Enhorabuena! Te has ganado el título de maniático de Friends, igualando la dedicación de Monica a la limpieza y el orden. Conoces los episodios tan bien como Ross conoce su sándwich de pavo sobrante. Cada escena, cada línea de diálogo y cada gesto de los amigos te resultan tan familiares como el aroma a café en Central Perk. Ahora que has alcanzado el nivel de maestría en Friends, cuéntanos, ¿cuál es tu momento favorito? ¿Con qué personaje te identificas más y por qué? ¡Estamos ansiosos por escuchar tus historias y celebrar juntos tu amor por esta icónica serie de televisión!";
+    } else if (finalScore >= 6 && finalScore <= 8) {
+        resultsTitle.innerText= `Unagi, Parece que Tienes los Sentidos Despiertos`
+        resultText.innerText = `"¡Bienvenido al club de los fans de Friends! Seguramente sabes deletrear correctamente 'Boscodictiasaur'. ¡Sí, comienza con M muda! Tu conocimiento sobre la serie es tan profundo como el amor de Joey por la pizza o la obsesión de Monica por la limpieza. Desde los momentos más divertidos hasta los más emotivos, has experimentado todas las emociones junto a Chandler, Monica, Rachel, Ross, Joey y Phoebe. Estás en sintonía con los latidos del corazón de Central Perk y te sientes como en casa en el apartamento de los amigos. Ahora que has demostrado tus habilidades en el mundo de Friends, prepárate para ser reconocido como un verdadero maestro de la serie."`;
+    } else if (finalScore >= 3 && finalScore <= 5) {
+        resultsTitle.innerText= `¡Parece que Has Comenzado a Ver la Luz`
+        resultText.innerText = `Bienvenido a la luz, amigo! Al igual que Chandler aprendiendo a bailar o Phoebe tocando la guitarra, estás descubriendo un nuevo mundo lleno de diversión y aprendizaje. A medida que avanzas en la serie, te encontrarás con momentos tan divertidos como cuando Ross grita '¡Pivot!' mientras intenta mover un sofá. Ahora que has dado los primeros pasos, te recomendamos los episodios con las lecciones más hilarantes y los momentos más icónicos de la serie. Prepárate para reír, llorar y celebrar la amistad como nunca antes.`;
+    } else {
+        resultsTitle.innerText= `Necesitas Urgentemente Ver Friends`
+        resultText.innerText = "Eres un verdadero novato en el mundo de Friends, tan nuevo como Joey en un juego de palabras. Pero no te preocupes, estamos aquí para guiarte en este viaje inolvidable. Sabes tanto de Friends como Phoebe de sueco o Rachel sobre capitales europeas, pero eso está a punto de cambiar. Prepárate para sumergirte en un mundo donde las referencias a Unagi y al sándwich de Ross son tan comunes como los cafés en Central Perk. Como recompensa por tu valentía al aventurarte en esta serie, te ofrecemos un masaje de Monica. Aunque, si conocieras a Monica, sabrías que no es realmente un premio, ¡sino más bien un castigo cómico! Así que, ¿estás listo para unirte a nosotros en este viaje?";
+    }
+    })
+
 
 nextButton.addEventListener("click", () => {
     currentQuestionIndex++;
