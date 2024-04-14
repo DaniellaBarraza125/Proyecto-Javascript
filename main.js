7a4910768bf1da806ae4d142e4287d9d163c62a1
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-04-14 10:08:23 +0200

    showing results

--- main.js
+++ main.js
@@ -6,24 +6,25 @@ const answerButtonsElement = document.getElementById("answer-buttons");
 const answerButton = document.getElementsByClassName("answerButton");
 const resultsButton = document.getElementById("results-btn")
 const questionContainer =document.getElementById("question-container")
-const restart = document.getElementById("restart")
-
+const scoreDiv = document.getElementById("score")
 const homeNav = document.getElementById("homeNav")
 const quizNav = document.getElementById("quizNav")
 const resultsNav = document.getElementById("resultsNav")
 const homeDiv = document.getElementById("homeDiv")
 const quizDiv = document.getElementById("quizDiv")
 const resultsDiv = document.getElementById ("resultsDiv")
+const resultText = document.getElementById("resultText")
 const API = "https://mocki.io/v1/e97adce9-e6cb-4ad9-8f31-0e3528b59e43";
 
 let quiz = [];
 let answer = [];
-let score = 0;
+let score;
+let finalScore;
 
 // console.log(resultsNav, homeNav, quizNav);
 
 const hideViews = (()=>{
-    console.log("working booetch");
+    // console.log("working booetch");
     homeDiv.classList.add("hide")
     questionContainer.classList.add("hide")
     resultsDiv.classList.add("hide")
@@ -33,29 +34,22 @@ hideViews();
 const showHome =()=>{
     hideViews();
     homeDiv.classList.remove("hide");
-    console.log("working");
+    // console.log("working");
 }
 
 const showQuiz =()=>{
     hideViews();
     questionContainer.classList.remove("hide");
-    console.log("working");
+    // console.log("working");
 }
 
-const showResults =()=>{
+const showResultsDiv =()=>{
     hideViews();
     resultsDiv.classList.remove("hide");
-    console.log("working");
+    // console.log("working");
 }
 
-
-homeNav.addEventListener("click",showHome)
-quizNav.addEventListener("click",showQuiz)
-resultsNav.addEventListener("click",showResults)
-
-
-axios.get(API)
-.then ((res)=> {quiz = res.data
+axios.get(API).then ((res)=> {quiz = res.data
 //si llega a dejar de funcionar esta API y debo cambiar a la anterior recuerda que seria res.data.response  
     // showQuestion(quiz)
     quiz.map(quiz => {
@@ -67,44 +61,39 @@ axios.get(API)
     });
     console.log("log en axios=", quiz)
     // console.log("trayend respuestas=", incorrectAnswers,correctAnswer);;
-})
-.catch((err)=>console.error(err))
+    }).catch((err)=>console.error(err))
 
 
 let currentQuestionIndex;
 const startGame = (()=>{
+    score = 0
     // console.log("funciono!");
     // console.log("respuesta incorrecta", incorrectAnswers)
     // console.log("correcta", correctAnswer); 
-
-
+    resultsButton.classList.add("hide");
     startButton.classList.add("hide");
     currentQuestionIndex = 0;
     questionContainerElement.classList.remove("hide");
     setNextQuestion();
 
 })
-
-startButton.addEventListener("click", startGame);
-
 const showQuestion = ((question)=>{
     resetState();
+    showScore();
 
+    
 // console.log("acceder a una pregunta=", quiz[1]);  
 questionElement.innerText= "";
 questionElement.innerText = question.question;
 //posicion al azar
-const correctAnswer = question.correct_answer;
-const incorrectAnswers = question.incorrect_answers;
+let correctAnswer = question.correct_answer;
 
     let allAnswers = [correctAnswer, ...incorrectAnswers];
     allAnswers.sort(() => Math.random() - 0.5);
     // console.log("allanswer", allAnswers);
 
-
 allAnswers.forEach(answer => {
     const button = document.createElement("button");
-    button.innerText = answer;
     button.innerText = answer;
     button.classList.add("answerButton")
     button.dataset.correct = answer === correctAnswer;
@@ -112,18 +101,13 @@ allAnswers.forEach(answer => {
     
     answerButtonsElement.appendChild(button);
 });
-
 console.log("Botón correcto: ", correctAnswer);
 console.log("current index ",currentQuestionIndex);
 })
-
-
 const setNextQuestion = (()=>{
     selectAnswer()
     showQuestion(quiz[currentQuestionIndex]);
 })
-
-
 const selectAnswer = () => {
     const selectedButton = event.target;
     countScore(selectedButton); 
@@ -146,31 +130,46 @@ const showButtons  = (()=>{
     startButton.classList.remove("hide");
     }
 });
-
-
+const showScore = (()=>{
+        let questionCount = currentQuestionIndex + 1;
+        let totalQuestions = quiz.length;
+        let questionDisplay = questionCount <= totalQuestions ? questionCount : totalQuestions;
+        scoreDiv.innerText = `Score = ${score}/10
+    Question = ${questionDisplay}/10`;
+    
+})
 const countScore=((element)=>{
     if (element.dataset.correct === "true") {
-        score += 1;
+        score ++;
         console.log("Score:", score);
     }
 })
-function resetState() {
+const resetState =(()=>{
     nextButton.classList.add("hide");
     answerButtonsElement.innerHTML = "";
-}
+})
+const showResults =(()=>{
+   showResultsDiv()
+    finalScore = score; // Asigna el valor de score a finalScore
+    resultText.innerText = `Tu puntaje final es ${finalScore} . `; 
+    //  if (score == 9) {console.log("yeeeeii");
+    // }
+    // else {
+    //     console.log("owww");
+    // }
+})
 
 nextButton.addEventListener("click", () => {
     currentQuestionIndex++;
     setNextQuestion();
     });
+    
+resultsButton.addEventListener("click", showResults)
+homeNav.addEventListener("click",showHome)
+quizNav.addEventListener("click",showQuiz)
+resultsNav.addEventListener("click",showResultsDiv)
+startButton.addEventListener("click", startGame);
 
-const results =(()=>{
-    if (score == 9) {console.log("yeeeeii");
-    }
-    else {
-        console.log("owww");
-    }
-})
 //genere esta funcion porque no se asignaba inmediatamente el valor correcto o falso pero ya no es necesaria
 // const checkButtons =(answerButtons, correctAnswer)=>{
 //     answerButtons.forEach(button => {
